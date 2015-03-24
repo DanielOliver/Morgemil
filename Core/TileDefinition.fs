dbb3b20792869ca9860fb14bc2fa001ecd228fd6
daniel_oliver <danieleugeneoliver@gmail.com>
2015-03-23 21:00:46 -0500

    Beginning of map generation. Starting with map tiles.

---
+++ Core/TileDefinition.fs
@@ -0,0 +1,9 @@
+﻿namespace Morgemil.Map
+
+//This defines a tile. Each tile instance will hold a reference to one of these.
+type TileDefinition(id : int, name : string, description : string, blocksMovement : bool, blocksSight : bool) = 
+  member this.ID = id
+  member this.Name = name
+  member this.Description = description
+  member this.BlocksMovement = blocksMovement
+  member this.BlocksSight = blocksSight
