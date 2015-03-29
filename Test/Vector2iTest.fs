e9b398ca568834f97ba531cb1b407658b1da94c2
daniel_oliver <danieleugeneoliver@gmail.com>
2015-03-28 19:56:48 -0500

    Basic Unit Tests. Requires NUnit 2.6.4

---
+++ Test/Vector2iTest.fs
@@ -0,0 +1,28 @@
+﻿module Morgemil.Test.Vector2iTest
+
+open Morgemil.Math
+open NUnit.Framework
+
+[<Test>]
+let ``Vector2i add zero Vector2i``() = 
+  let identity = Vector2i(5, 5)
+  Assert.AreEqual(Vector2i() + identity, identity)
+  Assert.AreEqual(identity + Vector2i(), identity)
+
+[<Test>]
+let ``Vector2i subtract zero Vector2i``() = 
+  let identity = Vector2i(5, 5)
+  Assert.AreEqual(identity - Vector2i(), identity)
+  Assert.AreNotEqual(Vector2i() - identity, identity)
+
+[<Test>]
+let ``Vector2i add zero scalar``() = 
+  let identity = Vector2i(5, 5)
+  Assert.AreEqual(0 + identity, identity)
+  Assert.AreEqual(identity + 0, identity)
+
+[<Test>]
+let ``Vector2i subtract zero scalar``() = 
+  let identity = Vector2i(5, 5)
+  Assert.AreEqual(identity - 0, identity)
+  Assert.AreNotEqual(0 - identity, identity)
