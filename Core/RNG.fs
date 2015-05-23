cee47aefc8ca1b7ddfe36306e113b970567b363f
daniel_oliver <danieleugeneoliver@gmail.com>
2015-05-23 13:35:25 -0500

    RNG Commit

---
+++ Core/RNG.fs
@@ -0,0 +1,20 @@
+﻿module Morgemil.Math.RNG
+
+open MathNet.Numerics.Random
+
+/// <summary>
+/// Type alias for a System.Random implementation
+/// </summary>
+type DefaultRNG = MersenneTwister
+
+let SeedRNG(seed : int) = DefaultRNG seed
+
+/// <summary>
+/// Given an RNG and likelihood, returns the success
+/// </summary>
+/// <param name="chance">[0.0, 1.0]</param>
+let Probability (rng : DefaultRNG) chance =
+  match chance with
+  | 0m -> false
+  | 1m -> true
+  | _ -> (rng.NextDecimal() >= chance)
