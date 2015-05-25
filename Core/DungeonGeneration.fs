78bfa8b6830fc4476b3777069cd7e9cf3a30046f
daniel_oliver <danieleugeneoliver@gmail.com>
2015-05-25 01:07:58 -0500

    Slight dungeon generation syntax cleaning

--- Core/DungeonGeneration.fs
+++ Core/DungeonGeneration.fs
@@ -8,19 +8,23 @@ open Morgemil.Math
 type private DungeonMap(roomSize : Rectangle) =
   let internal_map = Array.create roomSize.Area (TileDefinition.Default)
   member this.SetValue pos tile = internal_map.[roomSize.FlatCoord pos] <- tile
+  ///Return a single giant chunk to feed into the Visualizer
   member this.CreateChunk() = Chunk(roomSize, internal_map)
 
 ///Creates a Dungeon
 module DungeonGeneration =
+  ///The absolute minimum room area toleratred
   let private MinimumRoomArea = Rectangle(Vector2i(33, 33))
 
+  ///Randomizes a room's position and size within the defined area
   let private RandomizeRoom rng (maxArea : Rectangle) =
+    //Randomize size before placing the room randomly
     let room_size =
       MinimumRoomArea.Size + RNG.RandomVector rng (maxArea.Size - MinimumRoomArea.Size)
     let room_position = maxArea.Position + RNG.RandomVector rng (maxArea.Size - room_size)
     Rectangle(room_position, room_size)
 
-  ///Writes directly on the map (side-effects)
+  ///Writes directly on the map (side-effects). returns nothing
   let private GenerateRoom (dungeonMap : DungeonMap) (area : Rectangle) =
     let ChooseTile pt =
       match pt with
@@ -32,8 +36,11 @@ module DungeonGeneration =
     let rng = RNG.SeedRNG rngSeed
     //Hardcoded dungeon size
     let dungeon_size = Rectangle(Vector2i(512, 512))
-    let room_list =
-      BspGenerator.GenerateRoomDivides rng dungeon_size.Size |> List.map (RandomizeRoom rng)
+    //Empty map
     let dungeon_map = DungeonMap(dungeon_size)
-    room_list |> List.iter (GenerateRoom dungeon_map)
+    //BSP rooms with randomized size (Rectangle List). And feeds them to the map.
+    BspGenerator.GenerateRoomDivides rng dungeon_size.Size
+    |> List.map (RandomizeRoom rng)
+    |> List.iter (GenerateRoom dungeon_map)
+    //Return a chunk to feed to visualizer
     dungeon_map.CreateChunk()
--- Playground/Program.fs
+++ Playground/Program.fs
@@ -1,7 +1,7 @@
 ﻿[<EntryPoint>]
 let main argv =
   //let createdBspDungeon = Morgemil.Test.BspGenerator.GenerateRoomDivides 5 Morgemil.Math.Vector2i(500, 340)
-  let createdBspDungeon = [| Morgemil.Map.DungeonGeneration.Generate 56 |]
+  let createdBspDungeon = [| Morgemil.Map.DungeonGeneration.Generate 656556 |]
   let filename2 = "map_test2.bmp"
   let dungeonDraw2 = Morgemil.Test.DungeonVisualizer.Visualize(createdBspDungeon)
   dungeonDraw2.Save(filename2)
