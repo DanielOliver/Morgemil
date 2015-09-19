6a2eb456cc582a97aa302e90a5494612d2eab94f
daniel_oliver <danieleugeneoliver@gmail.com>
2015-09-19 17:20:42 -0500

    Rework EventMessage to EventRequest

--- Logic/Message.fs
+++ Logic/Message.fs
@@ -1,19 +1,19 @@
 ﻿namespace Morgemil.Logic
 
-///Discriminated union of all possible messages created
-type EventMessage = 
+///This represents a "request" for an action.
+type EventRequest = 
   | EntitySummoning
   | EntityFatality
   | EntityMovement
 
-///The results of an event
+///This represents the results of a request for an action
 type EventResult = 
   | EntitySummoned
   | EntityDeath
   | EntityMoved
 
-///Emits a new message
-type EventMessageEmit = EventMessage -> unit
+///Emits a new Request
+type EventRequestEmit = EventRequest -> unit
 
-///The callback to handle a message.
-type EventMessageHandler = EventMessageEmit -> EventMessage -> EventResult
+///The callback to handle a request and results.
+type EventMessageHandler = EventRequestEmit -> EventRequest -> EventResult
--- Logic/TurnQueue.fs
+++ Logic/TurnQueue.fs
@@ -4,21 +4,20 @@
 ///Example: The action of moving onto a tile with a trap causes a new message chain starting with the trap's activation.
 module TurnQueue = 
   let HandleMessages(handler : EventMessageHandler) = 
-    let _processedEvents = new System.Collections.Generic.List<EventMessage>()
-    let _eventQueue = new System.Collections.Generic.Queue<EventMessage>()
+    let _processedEvents = new System.Collections.Generic.List<EventRequest>()
+    let _eventQueue = new System.Collections.Generic.Queue<EventRequest>()
     let _eventResults = new System.Collections.Generic.List<EventResult>()
-    let _emit (message : EventMessage) = _eventQueue.Enqueue(message)
-    let one : EventMessageHandler = fun emit msg -> EventResult.EntitySummoned
+    let _emit message = _eventQueue.Enqueue(message)
     
     let rec _handle() = 
       match _eventQueue.Count with
       | 0 -> ()
       | _ -> 
-        let next = _eventQueue.Dequeue()
-        next
-        |> (handler _emit)
+        _eventQueue.Dequeue()
+        |> (fun request -> 
+        request |> _processedEvents.Add
+        request |> handler _emit)
         |> _eventResults.Add
-        next |> _processedEvents.Add
         _handle()
     _handle()
-    (List.ofSeq _processedEvents, List.ofSeq _eventResults)
+    (List.ofSeq _processedEvents, List.ofSeq _eventResults) //The processed messages are for development purposes. The eventResults matter though
