7df6f0fcd7e814f7868dd6799ce7531c4329f32d
daniel_oliver <danieleugeneoliver@gmail.com>
2015-09-19 17:06:21 -0500

    Turn queue now returns the processed events as well.

--- Logic/TurnQueue.fs
+++ Logic/TurnQueue.fs
@@ -4,6 +4,7 @@
 ///Example: The action of moving onto a tile with a trap causes a new message chain starting with the trap's activation.
 module TurnQueue = 
   let HandleMessages(handler : EventMessageHandler) = 
+    let _processedEvents = new System.Collections.Generic.List<EventMessage>()
     let _eventQueue = new System.Collections.Generic.Queue<EventMessage>()
     let _eventResults = new System.Collections.Generic.List<EventResult>()
     let _emit (message : EventMessage) = _eventQueue.Enqueue(message)
@@ -13,9 +14,11 @@ module TurnQueue =
       match _eventQueue.Count with
       | 0 -> ()
       | _ -> 
-        _eventQueue.Dequeue()
+        let next = _eventQueue.Dequeue()
+        next
         |> (handler _emit)
         |> _eventResults.Add
+        next |> _processedEvents.Add
         _handle()
     _handle()
-    (List.ofSeq _eventQueue, List.ofSeq _eventResults)
+    (List.ofSeq _processedEvents, List.ofSeq _eventResults)
