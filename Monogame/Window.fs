91199ac21db4ec06e8f39ab30dcfab38c6065909
daniel_oliver <danieleugeneoliver@gmail.com>
2015-12-05 15:23:59 -0600

    Monogame nuget package and initial commit

---
+++ Monogame/Window.fs
@@ -0,0 +1,27 @@
+﻿namespace Morgemil.Monogame
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type Window() as this = 
+  inherit Game()
+  do this.Content.RootDirectory <- "Content"
+  let graphics = new GraphicsDeviceManager(this)
+  let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+  
+  override x.Initialize() = 
+    spriteBatch <- new SpriteBatch(graphics.GraphicsDevice)
+    base.Initialize()
+    ()
+  
+  override x.LoadContent() = ()
+  override x.Update(gameTime) = ()
+  
+  override x.Draw(gameTime) = 
+    do x.GraphicsDevice.Clear Color.Black
+    ()
+  
+  ///Creates a new window and starts it
+  static member Start() = 
+    let window = new Window()
+    window.Run()
