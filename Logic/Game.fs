fd96d914a10bafdd4afd3565814e10937336dbcf
daniel_oliver <danieleugeneoliver@gmail.com>
2016-02-14 17:39:53 -0600

    Added resource system

--- Logic/Game.fs
+++ Logic/Game.fs
@@ -11,12 +11,20 @@ type Game(level : Level) =
       | EventResult.EntityMovementRequested(req) -> 
         let positionComponent = _world.Positions.[req.EntityId]
         let newPosition = positionComponent.Position + req.Direction
+        //If able to move, move
         if not (_world.Level.Tile(newPosition).BlocksMovement) && positionComponent.Mobile then 
           let newPositionComponent = { positionComponent with Position = newPosition }
           _world.Positions.[req.EntityId] <- newPositionComponent
           yield Message.PositionChange(positionComponent, newPositionComponent)
-          //TODO: Spend resources?
+          //Spend resources if possible
+          match _world.Resources.Find(req.EntityId) with
+          | Some(oldResources) -> 
+            let newResources = { oldResources with Stamina = oldResources.Stamina - 1.0<Stamina> }
+            _world.Resources.[req.EntityId] <- newResources
+            yield Message.ResourceChange(oldResources, newResources)
+          | None -> ()
           _world.Actions.Act(req.EntityId, 1.0<GameTime>)
+        //else dont move
         else _world.Actions.Act(req.EntityId, 0.0<GameTime>)
       | _ -> ()
     }
@@ -24,22 +32,7 @@ type Game(level : Level) =
   member this.HumanRequest(request : RequestedMovement) = 
     let nextAction = _world.Actions.Next()
     let results = TurnQueue.HandleMessages _handleRequest (EventResult.EntityMovementRequested request)
-    //TODO: Display results through gui
-    //    printfn ""
-    //    printfn "Current Entity %A" nextEntity.EntityId
-    //    printfn "Current time %f" _world.Actions.CurrentTime
     results |> Seq.iter (fun res -> printfn "%A" res)
-    //    let currentEntity() = _world.Entity(_world.Actions.Next.EntityId)
-    //    while not (currentEntity().Player.Value.IsHumanControlled) do
-    //      let ne = _world.Actions.StepToNext()
-    //      printfn ""
-    //      printfn "###########  BEGIN AI TURN"
-    //      printfn "Current time %f" _world.Actions.CurrentTime
-    //      printfn "%A" ne
-    //      _world.Actions.Act(ne.EntityId, 0.8<GameTime>)
-    //      printfn "###########  END AI TURN"
-    _world.Actions.[nextAction.EntityId] <- { nextAction with TimeOfNextAction = 
-                                                                nextAction.TimeOfNextAction + 1.0<GameTime>
-                                                              TimeOfRequest = nextAction.TimeOfNextAction }
+    _world.Entities.Free()
     printfn ""
     true
---
+++ Logic/ResourceSystem.fs
@@ -0,0 +1,10 @@
+﻿namespace Morgemil.Logic
+
+open Morgemil.Core
+
+type ResourceSystem(entitySystem : EntitySystem) = 
+  inherit EntityView<ResourceComponent>(entitySystem, ComponentType.Resouce, 
+                                        (fun t -> 
+                                        match t with
+                                        | Component.Resource(x) -> Some(x)
+                                        | _ -> None), Component.Resource)
