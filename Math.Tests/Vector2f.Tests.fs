bca23176ea3fa6826a0546aa98988bc46577f09b
daniel_oliver <danieleugeneoliver@gmail.com>
2017-02-12 11:42:44 -0600

    Fix overload issues

--- Math.Tests/Vector2f.Tests.fs
+++ Math.Tests/Vector2f.Tests.fs
@@ -10,25 +10,25 @@ let vec2 = Vector2f(-4.0, 2.0)
 [<Test>]
 let ``Test Vector2f Addition``() = 
   Assert.AreEqual(Vector2f(1.0, -3.0), vec1 + vec2)
-  Assert.AreEqual(Vector2f(7.0, -3.0), vec1 + 2)
-  Assert.AreEqual(Vector2f(7.0, -3.0), 2 + vec1)
+  Assert.AreEqual(Vector2f(7.0, -3.0), vec1 + 2.0)
+  Assert.AreEqual(Vector2f(7.0, -3.0), 2.0 + vec1)
   
 [<Test>]
 let ``Test Vector2f Subtraction``() = 
   Assert.AreEqual(Vector2f(9.0, -7.0), vec1 - vec2)
-  Assert.AreEqual(Vector2f(3.0, -7.0), vec1 - 2)
-  Assert.AreEqual(Vector2f(-3.0, 7.0), 2 - vec1)
+  Assert.AreEqual(Vector2f(3.0, -7.0), vec1 - 2.0)
+  Assert.AreEqual(Vector2f(-3.0, 7.0), 2.0 - vec1)
   
 [<Test>]
 let ``Test Vector2f Multiplication``() = 
   Assert.AreEqual(Vector2f(-20.0, -10.0), vec1 * vec2)
-  Assert.AreEqual(Vector2f(10.0, -10.0), vec1 * 2)
-  Assert.AreEqual(Vector2f(10.0, -10.0), 2 * vec1)
+  Assert.AreEqual(Vector2f(10.0, -10.0), vec1 * 2.0)
+  Assert.AreEqual(Vector2f(10.0, -10.0), 2.0 * vec1)
   
 [<Test>]
 let ``Test Vector2f Division``() = 
   Assert.AreEqual(Vector2f(-1.0, -2.0), vec1 / vec2)
-  Assert.AreEqual(Vector2f(2.0, -2.0), vec1 / 2)
+  Assert.AreEqual(Vector2f(2.0, -2.0), vec1 / 2.0)
   
 [<Test>]
 let ``Test Vector2f Methods``() = 
