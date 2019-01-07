54dc05d3f3183cd6ccb9229b1124cb213c1bebda
Daniel Oliver <11494375+DanielOliver@users.noreply.github.com>
2019-01-06 22:35:55 -0500

    Add tests to stress test relational table

--- src/Morgemil.Core.Tests/Relational.Tests.fs
+++ src/Morgemil.Core.Tests/Relational.Tests.fs
@@ -34,6 +34,8 @@ type ExampleTable() as this =
 let ``Can Add items to Index``() =
     let exampleTable = new ExampleTable()
 
+    Assert.Equal(0L, Table.GenerateKey exampleTable)
+
     let exampleItem1 = {
         ExampleRow.Attribute1 = 0
         ExampleKey = ExampleKey(500L)
@@ -47,16 +49,55 @@ let ``Can Add items to Index``() =
         Name = "Test1"
     }
     Table.AddRow exampleTable exampleItem2
-
+    
+    Assert.Equal(502L, Table.GenerateKey exampleTable)
     Assert.Equal(2, exampleTable |> Table.Items |> Seq.length)
     Assert.Throws<System.Exception>(fun () -> Table.GetRowByKey exampleTable 123L |> ignore) |> ignore
-    Assert.Equal(exampleItem1, Table.GetRowByKey exampleTable 500L)
-    
+    Assert.Equal(exampleItem1, Table.GetRowByKey exampleTable 500L)    
     Assert.Equal(2, (MultiIndex.GetRowsByKey exampleTable.NameIndex "Test1").Length)
     Assert.Empty(MultiIndex.GetRowsByKey exampleTable.NameIndex "Test1234234234")
     
     Table.RemoveRow exampleTable exampleItem1
-
     Assert.Equal(1, exampleTable |> Table.Items |> Seq.length)
     Assert.Throws<System.Exception>(fun () -> Table.GetRowByKey exampleTable 500L |> ignore) |> ignore
     Assert.Equal(1, (MultiIndex.GetRowsByKey exampleTable.NameIndex "Test1").Length)
+    
+    let exampleItem3 = {
+        exampleItem2 with
+            Name = "onetwo"
+    }
+    Table.AddRow exampleTable exampleItem3
+    Assert.Equal(exampleItem3, Table.GetRowByKey exampleTable 501L)
+    Assert.Empty(MultiIndex.GetRowsByKey exampleTable.NameIndex "Test1")
+    Assert.Equal(1, (MultiIndex.GetRowsByKey exampleTable.NameIndex "onetwo").Length)
+
+    for index in [1L..50_000L] do
+        {
+            ExampleRow.Attribute1 = int(index) * 2
+            ExampleKey = ExampleKey(index)
+            Name = "Index " + index.ToString()
+        }
+        |> Table.AddRow exampleTable
+        
+    Assert.Equal(50_000, exampleTable |> Table.Items |> Seq.length)
+
+    for item in exampleTable |> Table.Items |> Seq.toList do
+        {   item with
+                Name = item.Name + " Name"
+        }
+        |> Table.AddRow exampleTable
+        
+    Assert.Equal(50_000, exampleTable |> Table.Items |> Seq.length)
+    
+    for index in [1L..10_000L] do
+        index
+        |> Table.RemoveRowByKey exampleTable
+        
+    Assert.Equal(40_000, exampleTable |> Table.Items |> Seq.length)
+
+    for index in [1L..50_000L] do
+        index
+        |> Table.RemoveRowByKey exampleTable
+        
+    Assert.Equal(0, exampleTable |> Table.Items |> Seq.length)
+
--- src/Morgemil.Core/Relational.fs
+++ src/Morgemil.Core/Relational.fs
@@ -4,7 +4,8 @@ type IRow =
     abstract member Key : int64
     
 type IIndex<'tRow when 'tRow :> IRow> =
-    abstract member Add: 'tRow -> unit
+    abstract member AddRow: 'tRow -> unit
+    abstract member UpdateRow: 'tRow -> 'tRow -> unit
     abstract member Remove: 'tRow -> unit
     
 type IUniqueIndex<'tRow, 'tKey when 'tRow :> IRow> =
@@ -43,7 +44,13 @@ type UniqueIndex<'tRow, 'tKey when 'tKey: equality and 'tRow :> IRow>(getKey: 't
         
     interface IIndex<'tRow> with
         member this.Remove row = _dictionary.Remove (getKey row) |> ignore
-        member this.Add row = _dictionary.[ getKey row ] <- row
+        member this.AddRow row = _dictionary.[ getKey row ] <- row
+        member this.UpdateRow oldRow row =
+            let oldKey = getKey oldRow
+            let newKey = getKey row
+            if oldKey <> newKey then
+                _dictionary.Remove oldKey |> ignore
+            _dictionary.[ newKey ] <- row
 
 
 type MultiIndex<'tRow, 'tKey when 'tKey: equality and 'tRow :> IRow>(getKey: 'tRow -> 'tKey) =
@@ -55,11 +62,21 @@ type MultiIndex<'tRow, 'tKey when 'tKey: equality and 'tRow :> IRow>(getKey: 'tR
             match _dictionary.TryGetValue key with
             | true, value ->  _dictionary.[key] <- value |> List.filter(fun t -> t.Key <> row.Key)
             | _ -> ()
-        member this.Add row =
+        member this.AddRow row =
             let key = getKey row            
             match _dictionary.TryGetValue key with
-            | true, value -> _dictionary.[key] <- row :: (value |> List.filter(fun t -> t.Key <> row.Key))
+            | true, value -> _dictionary.[key] <- row :: value
             | _ -> _dictionary.[key] <- [ row ]
+        member this.UpdateRow oldRow row =
+            let oldKey = getKey oldRow
+            let newKey = getKey row
+            if oldKey = newKey then
+                _dictionary.[newKey] <- row :: (_dictionary.[newKey] |> List.filter(fun t -> t.Key <> row.Key))
+            else 
+            _dictionary.[oldKey] <- (_dictionary.[oldKey] |> List.filter(fun t -> t.Key <> row.Key))
+            match _dictionary.TryGetValue newKey with
+            | true, value -> _dictionary.[newKey] <- row :: (value |> List.filter(fun t -> t.Key <> row.Key))
+            | _ -> _dictionary.[newKey] <- [ row ]
 
     interface IMultiIndex<'tRow, 'tKey> with
         member this.TryGetRows(key: 'tKey): 'tRow list = 
@@ -80,7 +97,7 @@ type PrimaryIndex<'tRow when 'tRow :> IRow>() =
         member this.Items = _dictionary.Values |> Seq.map id
         member this.Remove row = _dictionary.Remove row.Key |> ignore
         member this.RemoveByKey key = _dictionary.Remove key |> ignore
-        member this.Add row = _dictionary.[ row.Key ] <- row        
+        member this.AddRow row = _dictionary.[ row.Key ] <- row
         member this.TryGetRow key = 
             match _dictionary.TryGetValue key with
             | true, value -> Some value
@@ -90,36 +107,59 @@ type PrimaryIndex<'tRow when 'tRow :> IRow>() =
                 match _dictionary.TryGetValue key with
                 | true, value -> value
                 | _ -> failwithf "Couldn't find key %A" key
-            and set key row = _dictionary.[ key ] <- row
-
+            and set key row = _dictionary.[ key ] <- row            
+        member this.UpdateRow oldRow row =
+            let oldKey = oldRow.Key
+            let newKey = row.Key
+            if oldKey <> newKey then
+                _dictionary.Remove oldKey |> ignore
+            _dictionary.[ newKey ] <- row
 
 type Table<'tRow when 'tRow :> IRow>() =
     let _primaryKey = new PrimaryIndex<'tRow>() :> IPrimaryIndex<'tRow>
     let _primaryKeyIndexCast = _primaryKey :> IIndex<'tRow>
     let mutable _indices = [ _primaryKeyIndexCast ]
+    let mutable _nextKey = 0L
+
+    let _setNextKey otherKey = _nextKey <- System.Math.Max(otherKey + 1L, _nextKey)
 
     member this.AddIndex index = _indices <- index :: _indices
     member this.PrimaryIndex = _primaryKeyIndexCast
+    member this.GenerateKey() =
+        let key = _nextKey
+        _nextKey <- _nextKey + 1L
+        key
     
     interface ITable<'tRow> with
         member this.Item
             with get key = _primaryKey.[ key ]
-            and set key row = (this :> ITable<'tRow>).Add row
+            and set key row = (this :> ITable<'tRow>).AddRow row
         member this.Items = _primaryKey.Items
         member this.Remove row = _indices |> List.iter(fun t -> t.Remove row)
         member this.RemoveByKey key =
             match _primaryKey.TryGetRow key with 
             | Some(row) -> _indices |> List.iter(fun t -> t.Remove row)
             | None -> ()
-        member this.Add row = _indices |> List.iter(fun t -> t.Add row)
+        member this.AddRow (row: 'tRow) =            
+            match _primaryKey.TryGetRow (row :> IRow).Key with
+            | Some(oldRow) -> 
+                _indices |> List.iter(fun t -> t.UpdateRow oldRow row)
+            | None -> 
+                _setNextKey (row :> IRow).Key
+                _indices |> List.iter(fun t -> t.AddRow row)
         member this.TryGetRow key = _primaryKey.TryGetRow key
+        member this.UpdateRow oldRow row =         
+            match _primaryKey.TryGetRow (row :> IRow).Key with
+            | Some(oldRow) -> 
+                _indices |> List.iter(fun t -> t.UpdateRow oldRow row)
+            | None -> ()
 
 module Table =
     let Items (table: 'T when 'T :> Table<'U>) =
         (table :> ITable<'U>).Items
 
     let AddRow (table: 'T when 'T :> Table<'U>) (row: 'U) =
-        (table :> ITable<'U>).Add(row)
+        (table :> ITable<'U>).AddRow(row)
 
     let GetRowByKey (table: 'T when 'T :> Table<_>) key =
         (table :> ITable<_>).Item key
@@ -132,6 +172,9 @@ module Table =
 
     let RemoveRowByKey (table: 'T when 'T :> Table<_>) key =
         (table :> ITable<'U>).RemoveByKey key
+
+    let GenerateKey (table: 'T when 'T :> Table<_>) =
+        table.GenerateKey()
 
 module MultiIndex =
     let GetRowsByKey (index: IMultiIndex<_,'T>) (key: 'T) =
