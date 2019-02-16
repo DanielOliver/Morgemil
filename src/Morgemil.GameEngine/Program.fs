582797241be2ddd1c279b2b00dfec0c16a54b110
Daniel Oliver <11494375+DanielOliver@users.noreply.github.com>
2019-02-16 03:56:30 -0500

    Display generated tile strategy

--- src/Morgemil.GameEngine/Program.fs
+++ src/Morgemil.GameEngine/Program.fs
@@ -1,15 +1,81 @@
 // Learn more about F# at http://fsharp.org
 
 open System
+open Morgemil.Core
+open Morgemil.Models
+open Morgemil.Math
+
+let defaultTile: Tile = {
+        ID = TileID 3L
+        Name = "Dungeon Wall"
+        TileType = TileType.Solid
+        Description = "Dungeon Walls are rock, solid, and carved from time."
+        BlocksMovement = true
+        BlocksSight = true
+        Tags = Map.empty
+        Representation = {
+            AnsiCharacter = '#'
+            ForegroundColor = Some <| Color.From(150, 150, 150)
+            BackGroundColor = Some <| Color.From(50, 50, 50)
+        }
+    }
+
+let floorTile: Tile = {
+        ID = TileID 4L
+        Name = "Dungeon Floor"
+        TileType = TileType.Ground
+        Description = "Dungeon floors are rock, paved cobblestone, and very slipper when bloody."
+        BlocksMovement = false
+        BlocksSight = false
+        Tags = Map.empty
+        Representation = {
+            AnsiCharacter = ' '
+            ForegroundColor = Some <| Color.From(50, 50, 50)
+            BackGroundColor = Some <| Color.From(255, 255, 255)
+        }
+    }
+
+let floorParameters: FloorGenerationParameter = {
+    Strategy = FloorGenerationStrategy.OpenFloor
+    Tiles = [|
+        defaultTile
+        floorTile
+    |]
+    SizeRange = Rectangle.create(10, 10, 15, 15)
+    Tags = Map.empty
+    DefaultTile = defaultTile
+    ID = FloorGenerationParameterID 5L
+}
+    
+
+
+type MapGeneratorConsole() =
+    inherit SadConsole.Console(40, 40)
+
+    let rng = RNG.SeedRNG(50)
+    let (tileMap, results) = FloorGenerator.Create floorParameters rng
+    let createColor (color: Morgemil.Math.Color) = Microsoft.Xna.Framework.Color(color.R, color.G, color.B, color.A)
+
+    do
+        for (position, tile, tileFeature) in tileMap.Tiles do
+            let backgroundColor = tile.Representation.BackGroundColor |> Option.map createColor |> Option.defaultValue Microsoft.Xna.Framework.Color.Black
+            let foregroundColor = tile.Representation.ForegroundColor |> Option.map createColor |> Option.defaultValue Microsoft.Xna.Framework.Color.White
+            base.Print(position.X, position.Y, tile.Representation.AnsiCharacter.ToString(), foregroundColor, backgroundColor)
+
+    member this.Zero = ()
+
 
 let Init() = 
-    SadConsole.Game.Instance.Components.Add(new SadConsole.Game.FPSCounterComponent(SadConsole.Game.Instance))
+    //SadConsole.Game.Instance.Components.Add(new SadConsole.Game.FPSCounterComponent(SadConsole.Game.Instance))
     SadConsole.Game.Instance.Window.Title <- "Morgemil";
+    SadConsole.Global.CurrentScreen <- (new MapGeneratorConsole())
+    //SadConsole.Global.CurrentScreen.Position <- Microsoft.Xna.Framework.Point(1,1)
+    ()
 
 [<EntryPoint>]
 let main argv =
     printfn "Starting Morgemil"
-    SadConsole.Game.Create(80, 25);
+    SadConsole.Game.Create("Cheepicus12.font", 80, 40);
     SadConsole.Game.OnInitialize <- new Action(Init)
     SadConsole.Game.Instance.Run();
     SadConsole.Game.Instance.Dispose();
