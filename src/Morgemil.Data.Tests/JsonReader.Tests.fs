4ef8727d3c7a10af986c1a03c5d511408c87b9ad
Daniel Oliver <danieloliver@users.noreply.github.com>
2019-04-28 15:03:49 -0400

    add serialization json test for defaultrng

--- src/Morgemil.Data.Tests/JsonReader.Tests.fs
+++ src/Morgemil.Data.Tests/JsonReader.Tests.fs
@@ -2,6 +2,7 @@ module Morgemil.Data.Tests.JsonReader.Tests
 
 
 open Morgemil.Data
+open Newtonsoft.Json
 open Xunit
 
 [<Fact>]
@@ -10,4 +11,15 @@ let ``Try Read Game``() =
     let rawDtoList = JsonReader.ReadGameFiles basePath
     Assert.Empty(rawDtoList.Errors)
     Assert.True(rawDtoList.Success)
-    
+
+[<Fact>]
+let ``Try serialize and deserialize JSON RNG``() =
+    let rng = Morgemil.Math.RNG.SeedRNG 500
+    rng.Next() |> ignore
+    let serialized = JsonConvert.SerializeObject(rng)
+    let expectedNext = rng.Next()
+    Assert.Equal(160506331, expectedNext)
+    let rngDeserialized = JsonConvert.DeserializeObject<Morgemil.Math.RNG.DefaultRNG>(serialized)
+    let actualNext = rngDeserialized.Next()
+    Assert.Equal(expectedNext, actualNext)
+
