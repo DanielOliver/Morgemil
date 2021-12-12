353057ef1cf9849d35bc680165681c019c63336f
Daniel <danieloliversoftware@gmail.com>
2021-12-11 20:13:00 -0500

    dotnet fake build

---
+++ build.fsx
@@ -0,0 +1,78 @@
+#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.initEnvironment ()
+
+Target.create "Clean" (fun _ ->
+  !! "src/**/bin"
+  ++ "src/**/obj"
+  ++ "src/**/TestResults"
+  ++ "coveragereport"
+  ++ "artifacts"
+  |> Shell.cleanDirs 
+
+  !! "src/**/coverage.xml*"
+  |> File.deleteAll
+)
+
+Target.create "Build" (fun _ ->
+  !! "src/**/*.*proj"
+  |> Seq.iter (DotNet.build (fun c ->
+        { c with
+            Configuration = DotNet.Release
+        })
+  )
+)
+
+Target.create "Test" (fun _ ->
+  !! "src/**/*Tests*proj"
+  |> Seq.iter (fun proj ->
+    [ 
+      "test"
+      "src"
+      "--configuration Release"
+      "/p:AltCoverForce=true"
+      "/p:AltCover=true"
+      "--logger"
+      "trx;LogFileName=testresults.trx"           
+    ]
+    |> CreateProcess.fromRawCommand "dotnet" 
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+  )
+)
+
+Target.create "Report" (fun _ ->
+  [ 
+    "reportgenerator"
+    "--configuration Release"
+    "-reports:**/coverage.xml"
+    "-targetdir:coveragereport";
+    "-reporttypes:\"HTML;Cobertura\"";
+    "-assemblyfilters:\"+Morgemil.*;-Morgemil.*.Tests\""     
+  ]
+  |> CreateProcess.fromRawCommand "dotnet"
+  |> CreateProcess.ensureExitCode
+  |> Proc.run
+  |> ignore
+)
+
+Target.create "All" ignore
+
+"Clean"
+  ==> "Build"
+  ==> "Test"
+  ==> "Report"
+  ==> "All"
+
+Target.runOrDefault "All"
