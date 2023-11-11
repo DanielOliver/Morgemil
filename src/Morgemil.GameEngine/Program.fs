4ee1414b5f7cd445228898e3ca629fbf867b2487
Daniel <danieloliversoftware@gmail.com>
2023-11-11 17:34:03 -0500

    NuGet package upgrades

--- src/Morgemil.GameEngine/Program.fs
+++ src/Morgemil.GameEngine/Program.fs
@@ -15,6 +15,7 @@ let StartMainStateMachine () =
         let container = ScreenContainer(ScreenGameState.SelectingScenario, mainGameState)
         SadConsole.Game.Instance.Screen <- container
         SadConsole.Game.Instance.DestroyDefaultStartingConsole()
+        SadConsole.Game.Instance.MonoGameInstance.Window.Title <- "Morgemil"
         SadConsole.Game.Instance.MonoGameInstance.WindowResized.Add(fun _ -> container.Reposition())
 
     SadConsole.Game.Create(60, 30, "Cheepicus12.font")
