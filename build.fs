8ee59f43b63ebf931a59329399f512e1e5fee3e8
Daniel <danieloliversoftware@gmail.com>
2024-05-05 12:47:19 -0400

    Process of elimination.

--- build.fs
+++ build.fs
@@ -22,6 +22,26 @@ Target.create "Clean" (fun _ ->
 
     !! "src/**/coverage.xml*" |> File.deleteAll)
 
+
+// The below error happens on Ubuntu image in GitHub Actions if not using the MSBuildParams below. I don't know why.
+//
+// Starting task 'DotNet:build': /home/runner/work/Morgemil/Morgemil/src/Morgemil.Console/Morgemil.Console.fsproj
+// > "/usr/bin/mono" --version (In: false, Out: true, Err: true)
+// /home/runner/work/Morgemil/Morgemil> "dotnet"  msbuild --version --nologo (In: false, Out: true, Err: true)
+// 17.9.8.16306
+// /home/runner/work/Morgemil/Morgemil> "dotnet"  build /home/runner/work/Morgemil/Morgemil/src/Morgemil.Console/Morgemil.Console.fsproj --configuration Release /clp:ForceConsoleColor /bl:/tmp/tmpBwfi72.tmp.binlog (In: false, Out: false, Err: false)
+// MSBuild version 17.9.8+b34f75857 for .NET
+//
+// Unhandled exception. Fake.Core.BuildFailedException: Target 'Build' failed.
+//  ---> System.AggregateException: One or more errors occurred. (Value cannot be null. (Parameter 'format'))
+//  ---> System.ArgumentNullException: Value cannot be null. (Parameter 'format')
+//    at System.ArgumentNullException.Throw(String paramName)
+//    at System.String.FormatHelper(IFormatProvider provider, String format, ReadOnlySpan`1 args)
+//    at System.String.Format(String format, Object[] args)
+//    at Microsoft.Build.Logging.StructuredLogger.BuildEventArgsReader.FormatResourceStringIgnoreCodeAndKeyword(String resource, String[] arguments) in C:\MSBuildStructuredLog\src\StructuredLogger\BinaryLogger\BuildEventArgsReader.Viewer.cs:line 198
+// ...
+// Error: Process completed with exit code 134.
+
 Target.create "Build" (fun _ ->
     !! "src/**/*.*proj"
     |> Seq.iter (
